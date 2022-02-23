53380f3159d825f3efcb611c39165e2083944a7e
Henrique Moraes <chmoraes9@gmail.com>
2022-02-23 17:42:09 -0300

    update

--- home/index.php
+++ home/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
     <script src="\node_modules\jquery\dist\jquery.min.js"></script>
     <link rel="stylesheet" href="css/template.css">
+    <script src="js/recursosBefore.js"></script>
     <title>Web Portfolio - Henrique Moraes</title>
 </head>
 <body>
@@ -26,27 +27,149 @@
     </header>
     <main>
         <div class="container-fluid">
-            <div class="row position-relative vh-100 align-items-center justify-content-center">
-                <div id="titulo" class="col-6 text-center">
-                    <h1 class="display-1">Portfolio dev</h1>
+            <div class="row position-relative vh-100 align-items-center justify-content-center" id="home">
+                <div id="titulo" class="col-10 col-sm-8 text-center">
+                    <h1 class="display-1">Portfólio dev</h1>
                     <p>Henrique Moraes</p>
                 </div>
-                    <div class="col-12 cores escopo esconderEscopo display-6 text-light py-3" id="aws">AWS</div>
-                    <div class="col-12 cores escopo esconderEscopo display-6 text-light py-3" id="pureCode">Pure Code</div>
-                    <div class="col-12 cores escopo esconderEscopo display-6 text-light py-3" id="wordPress">Wordpress</div>
+                <button class="col-12 text-start cores escopo esconderEscopo text-light py-1" id="AWS" onclick="exibirConteudo(this);">AWS</button>
+                <button class="col-12 text-end cores escopo esconderEscopo text-light py-1" id="Purecode" onclick="exibirConteudo(this);">Pure Code</button>
+                <button class="col-12 text-start cores escopo esconderEscopo text-light py-1" id="Wordpress" onclick="exibirConteudo(this);">Wordpress</butt>
             </div>
+            <div class="row pgs esconderConteudo" id="pgWordpress">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="px-4">                                
+                                <div class="d-flex justify-content-between">
+                                <h1>Wordpress</h1><button onclick="esconderConteudo(this);" class="d-flex align-items-start"><img src="img\close-322.svg" alt="Fechar"></button>
+                        </div>
+                                <p>
+                                    Minha experiência com o principal CMS do mercado abrange a criação de sites institucionais,
+                                    relatórios de sustentabilidade dinâmicos, lojas virtuais e portais de relacionamento com clientes.
+                                    Trabalho com o Elementor Builder quando o cliente pede, mas dou preferência para a criação de temas
+                                    à partir do zero, utilizando codex do Wordpress e seguindo as diretivas e boas práticas estipuladas por sua
+                                    equipe de desenvolvimento.
+                                    Os trabalhos que apresento a seguir são ficticios e foram criados com o único propósito de compor este portfólio.
+                                    Estou reiniciando minha carreira com o foco em desenvolvimento web rápido, seguro e barato. Minha experiência anterior
+                                    me trouxe até aqui. Agora, almejo novas oportunidades e desafios, empenhado e comprometido a criar e
+                                    manter uma carteira de relacionamentos de longo prazo.
+                                    </p>
+                                    <p class="text-end">
+                                        <strong>Obrigado pela oportunidade.</strong>
+                                    </p>
+                            </div>
+                            <div>
+                                <ul class="container">
+                                    <li>Acesse os sites clicando nas imagens</li>
+                                    <li class="row">
+                                        <div class="col-3 d-flex align-items-center">
+                                        <a href="https://esmalteria.hospedar.net">
+                                            <img src="img\EsmalteriaPadrão.png" alt="Esmlateria Padrão">
+                                        </a>
+                                        </div>
+                                        <div class="col-9">
+                                            <p>Site criado com base no <a href="https://esmalterianacional.com.br">esmalterianacional.com.br</a>,
+                                            buscando reproduzir os recursos técnicos e o layout. Responsivo, foi criado como tema preservando
+                                            as características nativas do wordpress, permitindo atualizações e expansões sem qualquer surpresa ou
+                                            incompatibilidade de plugins.</p>
+                                        </div>
+
+                                    </li>
+                                    <li class="row">
+                                        <div class="col-3 d-flex align-items-center">
+                                            <a href="https://imobiliaria.hospedar.net">
+                                                <img src="img\imobiliaria.png" alt="Imobiliária">
+                                            </a>
+                                        </div>
+                                        <div class="col-9">
+                                        <p>
+                                            Esta proposta foi realizada utilizando como referência o site <a href="https://www.remaxportalimoveis.com.br/">www.remaxportalimoveis.com.br/</a>.
+                                            Responsivo, o site possui um layout atrativo e uma boa trilha de navegação, oferecendo ao visitante, as informações
+                                            disponíveis com a relevância necessária, além de oferecer uma ferramenta de pesquisa que filtra os resultados e classifica para
+                                            exibição, de acordo com o modelo de negócio.
+                                        </p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row pgs esconderConteudo" id="pgPurecode">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="px-4">
+                                <div class="d-flex justify-content-between">
+                                <h1>Pure Code</h1><button onclick="esconderConteudo(this);" class="d-flex align-items-start"><img src="img\close-322.svg" alt="Fechar"></button>
+                                </div>
+                                <p>
+                                    O desenvolvimento de páginas utilizando código HTML, CSS e Javascript puros, consiste na construção do site
+                                    sem a utilização de um CMS ou framework de mercado.
+                                    É o modelo sugerido para sites estáticos, ou aplicativos com grandes camadas exclusivas, que encapsulem regras de negócio
+                                    complexas e restritas. Sites e aplicações construídos através de Pure Code tendem a ser mais caros, levam mais tempo para
+                                    serem desenvolvidos e permitem um nível de customização praticamente infinito.
+                                    Tem uma demanda para esse modelo de desenvolvimento?
+                                </p>
+                                <p class="text-end"><strong><a href="https://wa.me/5519984323300">Vamos conversar!</a></strong></p>
+                            </div>
+                            <div>
+                                <ul class="container">
+                                    <li>Acesse os sites clicando nas imagens</li>
+                                    <li class="row">
+                                        <div class="col-3 d-flex align-items-center">
+                                            <a href="https://onepage.hospedar.net"><img src="img\onepage.png" alt="Pure Code"></a>
+                                        </div>
+                                        <div class="col-9">
+                                            <p>
+                                                Criada como um primeiro trabalho onepage Pure Code, esse site tem um layout minimalista, que propõe ao visitante uma 
+                                                navegação fluida, transparente e rápida. É responsivo e intuitivo, sugerindo um estilo de navegação infinita que suporta
+                                                a inclusão de mais conteúdo, manual ou dinamicamente.
+                                                Um trabalho simples, mas que expõe o potencial do desenvolvimento Pure Code.
+                                            </p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row pgs esconderConteudo" id="pgAWS">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="px-4">
+                                <div class="d-flex justify-content-between">
+                                <h1>AWS</h1><button  onclick="esconderConteudo(this);" class="d-flex align-items-start"><img src="img\close-322.svg" alt="Fechar"></button>
+                                </div>
+                                <p>
+                                    A Amazon Web Services é uma empresa que faz parte do grupo Amazon Inc, que oferece serviços de computação em nuvem.
+                                    É uma excelente opção de hospedagem, entregando estabilidade, segurança e escalabilidade (possibilidade de crescimento vertical, 
+                                    em potência de recurso, além da escalabilidade horizontal, que é quantidade de recursos).
+                                    Eu tenho mais de 10 anos de experiência com a configuração e operação de ambientes linux na AWS, me posicionando como facilitador e
+                                    canal de suporte para aqueles que decidirem hospedar os seu sites e aplicações web com a Amazon.
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
         </div>
     </main>
     <footer class="fixed-bottom">
         <div class="container">
             <div class="row">
-                <div class="col-12 d-flex justify-content-center pb-4">
-                    <button id="itens" class="cores" onclick="exibirEscopo()"></button>
+                <div class="col-12 d-flex justify-content-center">
+                    <button id="itens" class="cores" onclick="exibirContato()"></button>
                 </div>
             </div>
         </div>
         <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
-        <script src="js/recursos.js"></script>
+        <script src="js/recursosAfter.js"></script>
     </footer>
 </body>
 </html>
---
+++ home/js/recursosBefore.js
@@ -0,0 +1,61 @@
+function exibirEscopo(){
+    document.querySelectorAll('.escopo').forEach(escopo =>{
+        escopo.classList.remove('esconderEscopo');
+        escopo.classList.add('exibirEscopo');
+
+    })
+}
+
+function esconderEscopo(){
+    document.querySelectorAll('.escopo').forEach(escopo =>{
+        escopo.classList.remove('exibirEscopo');
+        escopo.classList.add('esconderEscopo');
+
+    })
+}
+
+function esconderTitulo(){
+    document.getElementById('titulo').classList.remove('esconderEscopo');
+    document.getElementById('titulo').classList.add('exibirEscopo');
+}
+
+function exibirTitulo(){
+    document.getElementById('titulo').classList.remove('exibirEscopo');        
+    document.getElementById('titulo').classList.add('esconderEscopo');
+}
+
+function exibirContato(){
+    document.getElementById('contato').classList.remove('esconderEscopo');
+    document.getElementById('contato').classList.add('exibirEscopo');
+    exibirEscopo();
+    exibirTitulo();
+    document.getElementById('itens').onclick = function() {esconderContato()};
+}
+
+function esconderContato(){
+    document.getElementById('contato').classList.remove('exibirEscopo');
+    document.getElementById('contato').classList.add('esconderEscopo'); 
+    esconderEscopo();
+    esconderTitulo();
+    document.getElementById('itens').onclick = function() {exibirContato()};    
+}
+
+function exibirConteudo(conteudo){
+    const Conteudo = document.getElementById('pg' + conteudo.id);
+    Conteudo.style.zIndex=2;    
+    document.querySelector('#' + Conteudo.id + ' button').dataset.id = Conteudo.id;
+    esconderEscopo();    
+    Conteudo.classList.remove('esconderConteudo');
+    Conteudo.classList.add('exibirConteudo');
+    document.getElementById('itens').disabled = true;
+}
+
+function esconderConteudo(conteudo){
+    const Conteudo = conteudo.dataset.id;
+    document.querySelector('#' + Conteudo).classList.remove('exibirConteudo');
+    document.querySelector('#' + Conteudo).classList.add('esconderConteudo');
+    document.querySelector('#' + Conteudo).style.zIndex= -2;
+    esconderContato();
+    esconderEscopo();
+    document.getElementById('itens').disabled = false;
+}
