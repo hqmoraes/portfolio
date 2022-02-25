9458e45789cbcfb3ec34d98f9114469adee659dd
Henrique Moraes <chmoraes9@gmail.com>
2022-02-25 14:53:29 -0300

    update

--- home/js/recursosAfter.js
+++ home/js/recursosAfter.js
@@ -1,14 +1,3 @@
-function imprimirPagina(pagina){
-    console.log(pagina);
-}
-/*
-function exibirConteudo(pagina) {
-      httpRequest = new XMLHttpRequest();
-      httpRequest.onreadystatechange = imprimirPagina(pagina);
-      httpRequest.open('GET',pagina, true);
-      httpRequest.send();
-    }
 
-    */
 
     
