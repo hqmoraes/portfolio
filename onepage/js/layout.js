453a7c3182b0159e7e89ad3cc6df89878473d8af
Henrique Moraes <chmoraes9@gmail.com>
2022-02-27 11:12:16 -0300

    update

--- onepage/js/layout.js
+++ onepage/js/layout.js
@@ -56,5 +56,5 @@ paginasFim.forEach(p =>{
 
     $('#fim footer').append('<div>The first project</div>')
 
-    $('#home').css('background: url(/img/bg/fundoParte1-20.jpg), white;');
+    $('#home').css({'background': 'url(/img/bg/fundoParte1-20.jpg), white'});
     console.log(wsz);
