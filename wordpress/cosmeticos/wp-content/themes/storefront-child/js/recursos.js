8ead6f124a2f0ea3e0d1774a1b7646ffddeb8d8e
hqmoraes <hmoraes@webhost.hospedar.net>
2022-03-02 17:37:56 +0000

    update

--- wordpress/cosmeticos/wp-content/themes/storefront-child/js/recursos.js
+++ wordpress/cosmeticos/wp-content/themes/storefront-child/js/recursos.js
@@ -2,3 +2,4 @@ var wtp = document.createElement('div');
 wtp.style.backgroundImage = 'http://cosmeticos.hospedar.net/wp-content/uploads/2022/03/Whats.svg';
 wtp.classList.add('wtp');
 document.body.appendChild(wtp);
+console.log('teste');
