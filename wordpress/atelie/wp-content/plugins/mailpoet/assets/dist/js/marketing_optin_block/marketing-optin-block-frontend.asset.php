ea601e7451223d5d779aa8e898ff63cf0be4a3a8
Henrique Moraes <chmoraes9@gmail.com>
2022-03-10 15:57:33 -0300

    update

--- wordpress/atelie/wp-content/plugins/mailpoet/assets/dist/js/marketing_optin_block/marketing-optin-block-frontend.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!defined('ABSPATH')) exit;
-
- return array('dependencies' => array('wc-blocks-checkout', 'wc-settings', 'wp-element', 'wp-polyfill'), 'version' => '9bb4a90e2d30b7567ae475dd7b13e33f');
